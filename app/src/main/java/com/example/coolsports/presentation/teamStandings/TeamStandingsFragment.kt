7f1defc47210c1245779f02066b5dac9e3eea15c
RashaMishaalAbed <rasha.mishaal.abed@gmail.com>
2022-12-19 17:39:41 +0400

    casting issue

--- app/src/main/java/com/example/coolsports/presentation/teamStandings/TeamStandingsFragment.kt
+++ app/src/main/java/com/example/coolsports/presentation/teamStandings/TeamStandingsFragment.kt
@@ -19,17 +19,21 @@ import com.example.coolsports.domain.model.league.BaseLeagueInfo
 import com.example.coolsports.domain.model.league.LeagueData01
 import com.example.coolsports.domain.model.league.LeagueData04
 import com.example.coolsports.domain.model.league.LeagueEntity
+import com.example.coolsports.domain.model.leagueStandings.LeagueStandingGroup.GroupScore
 import com.example.coolsports.domain.model.leagueStandings.LeagueStandingGroup.LeagueStandingsGroupBase
+import com.example.coolsports.domain.model.leagueStandings.LeagueStandingGroup.ScoreItem
 import com.example.coolsports.domain.model.leagueStandings.LeagueStandingsBase
+import com.example.coolsports.domain.model.leagueStandings.TotalStanding
 import com.example.coolsports.presentation.league.LeagueViewModel
 import com.example.coolsports.presentation.playerStandings.PlayerStandingsViewModel
 import com.google.gson.Gson
+import com.google.gson.internal.LinkedTreeMap
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import kotlin.math.log
 
 @AndroidEntryPoint
-class TeamStandingsFragment(val rules: LeagueData04) : Fragment() {
+class TeamStandingsFragment(val rules: LeagueData04,var leagueStanding: List<Any>,val leagueId: Int) : Fragment() {
 
     val TAG: String = "TeamStandingsFragment"
     private var _binding: FragmentTeamStandingsBinding? = null
@@ -38,6 +42,7 @@ class TeamStandingsFragment(val rules: LeagueData04) : Fragment() {
     var bundle = Bundle()
     var leagueList = listOf<Any>()
     private val viewModel by viewModels<TeamStandingsViewModel>()
+    private lateinit var teamStandingAdapter: TeamStandingAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -56,6 +61,35 @@ class TeamStandingsFragment(val rules: LeagueData04) : Fragment() {
 
 
         binding.rulesValue.text = rules.ruleEn
+        Log.d(TAG, "leagueStanding====== $leagueStanding")
+
+
+      //  ((leagueStanding as ArrayList<*>)[0] as LinkedTreeMap<*,*>).containsKey("list")
+
+        if (leagueStanding.filterIsInstance<List<LeagueStandingsBase>>().isNotEmpty()){
+            binding.teamStandingContainer.visibility = View.VISIBLE
+            binding.topThree.visibility = View.VISIBLE
+           // binding.firstTeamName.text =
+            val totalStanding = leagueStanding.filterIsInstance<LeagueStandingsBase>()[0].totalStandings
+            val teamInfo = leagueStanding.filterIsInstance<LeagueStandingsBase>()[0].leagueInfo
+            binding.teamRecyclerView.adapter= TeamStandingAdapter(requireContext(), object : OnTeamClickListener {
+                 override fun onClickListener(totalStanding: TotalStanding) {
+
+                 }
+             },teamInfo,totalStanding)
+
+
+        }else if (((leagueStanding as ArrayList<*>)[0] as LinkedTreeMap<*,*>).containsKey("list")){
+            binding.topThree.visibility = View.GONE
+            binding.teamStandingContainer.visibility = View.GONE
+            binding.teamStandingGroupContainer.visibility = View.VISIBLE
+            val groupScore = ((((((((leagueStanding as ArrayList<*>)[0] as LinkedTreeMap<*,*>)["list"] as ArrayList<*> )[0] as LinkedTreeMap<*,*>)["score"] as ArrayList<*>)[0] as LinkedTreeMap<*,*>)["groupScore"]) as ArrayList<*>)
+//            binding.teamRecyclerGroupView.adapter= TeamStandingGroupAdapter(requireContext(), object : OnGroupItemsClickListener {
+//                override fun onClickListener(item: ScoreItem) {
+//
+//                }
+//            },groupScore)
+        }
 
 
     }
--- app/src/main/java/com/example/coolsports/presentation/teamStandings/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/coolsports/presentation/teamStandings/ViewPagerAdapter.kt
@@ -5,7 +5,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.coolsports.domain.model.league.LeagueData04
 import com.example.coolsports.presentation.playerStandings.PlayerStandingsFragment
 
-class ViewPagerAdapter(val fragment: Fragment,val rules: LeagueData04) : FragmentStateAdapter(fragment){
+class ViewPagerAdapter(val fragment: Fragment,val rules: LeagueData04,  var leagueStanding: List<Any>, val leagueId: Int) : FragmentStateAdapter(fragment){
     private val NUM_ITEMS = 2
     override fun getItemCount(): Int {
         return NUM_ITEMS
@@ -13,9 +13,9 @@ class ViewPagerAdapter(val fragment: Fragment,val rules: LeagueData04) : Fragmen
 
     override fun createFragment(position: Int): Fragment {
         return when(position){
-            0 -> TeamStandingsFragment(rules)
+            0 -> TeamStandingsFragment(rules,leagueStanding,leagueId)
             1 -> PlayerStandingsFragment()
-            else -> TeamStandingsFragment(rules)
+            else -> TeamStandingsFragment(rules,leagueStanding,leagueId)
         }
     }
 
